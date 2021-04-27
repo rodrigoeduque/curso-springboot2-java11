3a24c0a25a9362ad513cc96a00e4582573cf1291
Rodrigo Eustáquio Duque <32581219+rodrigoeduque@users.noreply.github.com>
2021-04-27 18:26:35 -0300

    Payment, one-to-one association

--- src/main/java/br/com/rodrigoeduque/curso/config/TestConfig.java
+++ src/main/java/br/com/rodrigoeduque/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.rodrigoeduque.curso.entities.Category;
 import br.com.rodrigoeduque.curso.entities.Order;
 import br.com.rodrigoeduque.curso.entities.OrderItem;
+import br.com.rodrigoeduque.curso.entities.Payment;
 import br.com.rodrigoeduque.curso.entities.Product;
 import br.com.rodrigoeduque.curso.entities.User;
 import br.com.rodrigoeduque.curso.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(order3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), order1);
+		order1.setPayment(pay1);
+		
+		orderRepository.save(order1);
 	}
 }
